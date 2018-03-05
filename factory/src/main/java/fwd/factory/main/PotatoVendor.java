270a1ae5a45c40c9f36b910f95c808baa978e0d2
Benjamin <benjamin.labas@gmail.com>
2018-03-05 23:16:45 +0100

    move factory-scala to factory and delete old factory

--- factory/src/main/java/fwd/factory/main/PotatoVendor.java
+++
@@ -1,9 +0,0 @@
-package fwd.factory.main;
-
-import fwd.common.main.PotatoDelivery;
-import fwd.common.main.PotatoOrder;
-
-public interface PotatoVendor {
-
-    PotatoDelivery purchase(PotatoOrder order);
-}
